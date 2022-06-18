6e6d12c16d6f6e83d32c980990d58dead579eedd
Aditya <adigowdtham@gmail.com>
2022-06-18 15:51:55 +0530

    Project Created and the program runs successfully.

---
+++ Graded_Assessment-1_OOPS/src/department/child/HrDepartment.java
@@ -0,0 +1,21 @@
+package department.child;
+
+import department.module.SuperDepartment;
+
+public class HrDepartment extends SuperDepartment {
+	public String departmentName() {
+		return "HR Department";
+	}
+
+	public String getTodaysWork() {
+		return "Fill today's worksheet and mark your attendence";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
---
+++ Graded_Assessment-1_OOPS/src/department/driver_class/Main.java
@@ -0,0 +1,38 @@
+package department.driver_class;
+
+import department.child.AdminDepartment;
+
+import department.child.HrDepartment;
+
+import department.child.TechDepartment;
+
+public class Main {
+	public static void main(String[] args) {
+
+		AdminDepartment obj1 = new AdminDepartment();
+		System.out.println("Welcome to " + obj1.departmentName());
+		System.out.println(obj1.getTodaysWork());
+		System.out.println(obj1.getWorkDeadline());
+		System.out.println(obj1.isTodayAHoliday());
+
+		System.out.println();
+
+		HrDepartment obj2 = new HrDepartment();
+		System.out.println("Welcome to " + obj2.departmentName());
+		System.out.println(obj2.doActivity());
+		System.out.println(obj2.getTodaysWork());
+		System.out.println(obj2.getWorkDeadline());
+		System.out.println(obj2.isTodayAHoliday());
+
+		System.out.println();
+
+		TechDepartment obj3 = new TechDepartment();
+		System.out.println("Welcome to " + obj3.departmentName());
+		System.out.println(obj3.getTodaysWork());
+		System.out.println(obj3.getWorkDeadline());
+		System.out.println(obj3.getTechStackInformation());
+		System.out.println(obj3.isTodayAHoliday());
+
+	}
+
+}
